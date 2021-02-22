8739dd6d0a4507ce3af1899f07a04e988733cfdb
NEXTru86 <kharinpav86@gmail.com>
2021-02-22 20:53:02 +0200

    Выполнил ДЗ №8

--- js/gallery.js
+++ js/gallery.js
@@ -2,6 +2,10 @@ import items from "./gallery-items.js"
 
 const refs = {
     gallery: document.querySelector('.js-gallery'),
+    background: document.querySelector('.js-lightbox'),
+    backgroundImg: document.querySelector('.lightbox__image'),
+    closeLightBoxBtn: document.querySelector('button[data-action="close-lightbox"]'),
+    lightBoxOverlay: document.querySelector('.lightbox__overlay'),
 };
 
 const createImegeItem = item => {
@@ -26,6 +30,37 @@ const createImegeItem = item => {
 }
 
 const createGallery = items.map(item => createImegeItem(item));
-
 refs.gallery.append(...createGallery);
+
+refs.gallery.addEventListener('click', onGalleryClick);
+
+function onGalleryClick(event) {
+    event.preventDefault();
+
+    const imageRef = event.target;
+  
+    if (imageRef.nodeName !== 'IMG') {
+        return;
+    };
+
+    refs.background.classList.add('is-open');
+    refs.backgroundImg.src = imageRef.dataset.source;
+    refs.backgroundImg.alt = imageRef.alt;
+};
+
+refs.closeLightBoxBtn.addEventListener('click', onCloseLightBox);
+refs.lightBoxOverlay.addEventListener('click', onCloseLightBox);
+window.addEventListener('keydown', onCloseLightBox);
+
+function onCloseLightBox(event) {
+    refs.background.classList.remove('is-open');
+    refs.backgroundImg.src = '';
+    refs.backgroundImg.alt = '';
+
+    if (event.code === 'Escape') {
+        refs.background.classList.remove('is-open');
+        refs.backgroundImg.src = '';
+        refs.backgroundImg.alt = '';
+    };
+};
 
